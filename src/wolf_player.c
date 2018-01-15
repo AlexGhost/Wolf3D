e17f463cda320906a9b318ae5926a40f5e9d23f4
acourtin <acourtin@student.42.fr>
2018-01-15 15:51:21 +0100

    norm

--- src/wolf_player.c
+++ src/wolf_player.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/14 16:23:12 by acourtin          #+#    #+#             */
-/*   Updated: 2018/01/15 15:48:18 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/01/15 15:50:29 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,10 @@ void			wolf_player_loop(t_wolf *wolf_game)
 {
 	if (wolf_game->player.is_moving != 0)
 	{
-		wolf_game->player.posx += (wolf_game->player.is_moving * PLAYER_SPEED * (int)(wolf_game->player.rotx * 10));
-		wolf_game->player.posy += wolf_game->player.is_moving * PLAYER_SPEED * (int)(wolf_game->player.roty * 10);
+		wolf_game->player.posx += (wolf_game->player.is_moving * PLAYER_SPEED \
+			* (int)(wolf_game->player.rotx * 10));
+		wolf_game->player.posy += wolf_game->player.is_moving * PLAYER_SPEED \
+			* (int)(wolf_game->player.roty * 10);
 	}
 	if (wolf_game->player.is_rot != 0)
 		wolf_game->player.rot += wolf_game->player.is_rot * PLAYER_TURN_RATE;
