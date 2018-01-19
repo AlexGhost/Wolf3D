127fae74c8407e45eb5fe1816d82610f5d5a0234
acourtin <acourtin@student.42.fr>
2018-01-20 00:56:36 +0100

    tweaky bastard

--- src/wolf_world.c
+++ src/wolf_world.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/18 18:32:01 by acourtin          #+#    #+#             */
-/*   Updated: 2018/01/20 00:17:45 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/01/20 00:56:00 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,14 @@ static void			wolf_draw_skybox(t_wolf *wolf_game)
 {
 	int		i;
 	int		j;
-	char	*c;
 
 	clear_window(wolf_game);
-	j = -1;
-	while (++j < 400)
-	{
-		i = -1;
-		while (++i < 1400)
-			wolf_game->smlx.imgstr[i + (j * WIN_WIDTH)] = \
-				wolf_game->xpm_skybox.imgstr[i + (j * 1400)];
-	}
-	j = WIN_HEIGHT / 2 - 1;
-	while (++j < WIN_HEIGHT)
-	{
-		i = -1;
-		while (++i < WIN_WIDTH)
-			wolf_game->smlx.imgstr[i + (j * WIN_WIDTH)] = COLOR_FLOOR;
-	}
+	i = -1;
+	while (++i < (WIN_WIDTH * WIN_HEIGHT) / 2)
+		wolf_game->smlx.imgstr[i] = wolf_game->xpm_skybox.imgstr[i];
+	i = (WIN_HEIGHT * WIN_WIDTH) / 2 - 1;
+	while (++i < WIN_HEIGHT * WIN_WIDTH)
+		wolf_game->smlx.imgstr[i] = COLOR_FLOOR;
 }
 
 void				wolf_draw_world(t_wolf *wolf_game)
