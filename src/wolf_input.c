ed829d333c9f1672ff59495beb21210b031f992b
acourtin <acourtin@student.42.fr>
2018-02-13 18:06:32 +0100

    can no more shoot when in minimap mode

--- src/wolf_input.c
+++ src/wolf_input.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/14 13:30:46 by acourtin          #+#    #+#             */
-/*   Updated: 2018/02/12 20:53:17 by acourtin         ###   ########.fr       */
+/*   Updated: 2018/02/13 18:05:27 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int				wolf_keyrelease_event(int keycode, t_wolf *wolf_game)
 			|| keycode == BUTTON_UP || keycode == BUTTON_DOWN)
 		wolf_game->player.is_moving = 0;
 	else if (keycode == BUTTON_SPACE && !(wolf_game->player.is_firing == 1) \
-		&& wolf_game->player.ammo > 0)
+		&& wolf_game->player.ammo > 0 && !(wolf_game->draw_minimap == 1))
 	{
 		wolf_game->player.is_firing = 1;
 		wolf_game->player.ammo--;
